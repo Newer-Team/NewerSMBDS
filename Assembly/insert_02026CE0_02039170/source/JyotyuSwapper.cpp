384de6b6b973f21e793515a076e35991a48d6410
skawo <43761362+skawo@users.noreply.github.com>
2022-02-22 14:55:09 +0100

    Update JyotyuSwapper.cpp

--- Assembly/insert_02026CE0_02039170/source/JyotyuSwapper.cpp
+++ Assembly/insert_02026CE0_02039170/source/JyotyuSwapper.cpp
@@ -2,51 +2,45 @@
 #include "VariousFunctions.h"
 
 void nsub_020A16A8_ov_00(int view)
-	{
-		int fileoffset = getUnk3ForView(view);
+{
+	int fileoffset = getUnk3ForView(view);
 
-		if (fileoffset == 255)	loadFileByExtId_LZ_Dest(2643-0x83, (u16*)0x6000000);
-		else loadFileByExtId_LZ_Dest(2738-0x83 +fileoffset, (u16*)0x6000000);
+	if (fileoffset == 255)	
+		loadFileByExtId_LZ_Dest(2643-0x83, (u16*)0x6000000);
+	else 
+		loadFileByExtId_LZ_Dest(2738-0x83 +fileoffset, (u16*)0x6000000);
 
-		if (fileoffset == 255) fileoffset = 3;
+	if (fileoffset == 255) 
+		fileoffset = 3;
 		
-		loadFileByExtId_LZ_Dest(2744-0x83 +fileoffset, (u16*)ptrToAnimatedJyotyu);						
+	loadFileByExtId_LZ_Dest(2744-0x83 +fileoffset, (u16*)ptrToAnimatedJyotyu);						
 		
-		u8* ptr = (u8*) allocFromGameHeap(1024);					
-		loadFileByExtId_LZ_Dest(2750-0x83 +fileoffset, ptr);
+	u8* ptr = (u8*) allocFromGameHeap(1024);					
+	loadFileByExtId_LZ_Dest(2750-0x83 +fileoffset, ptr);
 	
-		DC_FlushRangeOrAll(ptr, 0x400);									//Sprites palette
-		GX_BeginLoadOBJExtPltt();
-		GX_LoadOBJExtPltt(ptr, 0x0, 0x400);
-		GX_EndLoadOBJExtPltt();
+	DC_FlushRangeOrAll(ptr, 0x400);									//Sprites palette
+	GX_BeginLoadOBJExtPltt();
+	GX_LoadOBJExtPltt(ptr, 0x0, 0x400);
+	GX_EndLoadOBJExtPltt();
 			
-		DC_FlushRangeOrAll(ptr, 0x400);									//Tileset palette
-		GX_BeginLoadBGExtPltt();
-		GX_LoadBGExtPltt(ptr, 0x4000, 0x400);
-		GX_EndLoadBGExtPltt();
-		freeToGameHeap(ptr);
+	DC_FlushRangeOrAll(ptr, 0x400);									//Tileset palette
+	GX_BeginLoadBGExtPltt();
+	GX_LoadBGExtPltt(ptr, 0x4000, 0x400);
+	GX_EndLoadBGExtPltt();
+	freeToGameHeap(ptr);
 	
-//		ptr = (u8*) allocFromGameHeap(1024);
-//		loadFileByExtId_LZ_Dest(2725-0x83, ptr);		
+//	ptr = (u8*) allocFromGameHeap(1024);
+//	loadFileByExtId_LZ_Dest(2725-0x83, ptr);		
 		
-//		DC_FlushRangeOrAll(ptr, 0x400);									//New Palette
-//		GX_BeginLoadOBJExtPltt();
-//		GX_LoadOBJExtPltt(ptr, 0x1000, 0x400);
-//		GX_EndLoadOBJExtPltt();
-//		freeToGameHeap(ptr);
+//	DC_FlushRangeOrAll(ptr, 0x400);									//New Palette
+//	GX_BeginLoadOBJExtPltt();
+//	GX_LoadOBJExtPltt(ptr, 0x1000, 0x400);
+//	GX_EndLoadOBJExtPltt();
+//	freeToGameHeap(ptr);
 		
-		loadFileByExtId_LZ_Dest(2726-0x83 +fileoffset, (u16*)0x6405E00);		//Sprites
-		
-		loadFileByExtId_LZ_Dest(2732-0x83 +fileoffset, (u16*)0x6408200);		//Palette
-		
-		loadFileByExtId_Cached_intoObjPalette(0x69F, 0x400);					//Enemy Palette
-	
-		return;
-	}
-	
+	loadFileByExtId_LZ_Dest(2726-0x83 +fileoffset, (u16*)0x6405E00);		//Sprites
+	loadFileByExtId_LZ_Dest(2732-0x83 +fileoffset, (u16*)0x6408200);		//Palette	
+	loadFileByExtId_Cached_intoObjPalette(0x69F, 0x400);				//Enemy Palette
 
-
-	
-
-
-	
+	return;
+}
